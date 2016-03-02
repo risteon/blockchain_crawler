d7b0dcd378d59c9637cc890681eabe958de2b562
Christoph B. Rist <rist.christoph@gmail.com>
2016-03-02 22:02:09 +0100

    Empty initial project

---
+++ tests/test_blockchain_crawler.py
@@ -0,0 +1,13 @@
+import unittest
+
+from context import blockchain_crawler
+
+
+class TestBlockChainCrawler(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
